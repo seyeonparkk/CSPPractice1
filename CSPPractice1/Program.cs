812e88bd6387ea3fea64b433e9a78d702c83b3f9
Seyeon Park <psyvitamin@gmail.com>
2024-07-01 13:59:35 +0900

    #5 22-7. 소멸자

--- CSPPractice1/Program.cs
+++ CSPPractice1/Program.cs
@@ -20,12 +20,12 @@ namespace CSPractice1
         {
             Console.WriteLine("(인스턴스)생성자 호출");
         }
+
         ~Sample()
         {
             Console.WriteLine("소멸자 호출");
         }
     }
-}
 
     class MyMath
     {
@@ -240,6 +240,9 @@ namespace CSPractice1
             Sample sample = new Sample();
             Console.WriteLine("세 번째 위치");
 
-        // #5 22-7. 소멸자
+            // #5 22-7. 소멸자
+
+
+        }
     }
 }
